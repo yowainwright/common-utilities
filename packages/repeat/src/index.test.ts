32b7f22641d041d58fb63d816096ae81df61ea87
yowainwright <yowainwright@gmail.com>
2020-09-21 22:01:38 -0700

    fix(root): updates eslint/prettier config 😍

--- packages/repeat/src/index.test.ts
+++ packages/repeat/src/index.test.ts
@@ -3,7 +3,7 @@ import repeat from './index'
 describe('@common-utilities/', () => {
   describe('repeat', () => {
     test('it repeats', () => {
-      const add1 = (x) => x + 1
+      const add1 = (x: number): number => x + 1
       const result = repeat(100)(add1)(0)
       expect(result).toEqual(100)
     })
