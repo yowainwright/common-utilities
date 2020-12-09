1d3f9962391f409144a13dc56e885e990834fe23
yowainwright <yowainwright@gmail.com>
2020-12-08 23:08:35 -0800

    feat(wait-until-defined): init tests

---
+++ packages/wait-until-defined/src/index.test.ts
@@ -0,0 +1,16 @@
+import { wait, isDefined, checkDefinition, waitUntilDefined } from './index'
+
+describe('@common-utilities/', () => {
+  describe('wait-until-defined', () => {
+    describe('wait', () => { 
+
+    })
+    
+    describe('isDefined', () => { 
+
+    })
+
+    describe('checkDefinition', () => { })
+
+    describe('waitUntilDefined', () => { });
+})
