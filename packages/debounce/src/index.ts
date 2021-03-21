fb2c7c1c2f04c36148053b0802cf1a811118a66f
yowainwright <yowainwright@gmail.com>
2021-03-20 17:41:30 -0700

    feat(debounce): adds initial debounce work 🏓

---
+++ packages/debounce/src/index.ts
@@ -0,0 +1,16 @@
+type Fn = (args?: unknown) => void
+
+/**
+ * debounce 🏓
+ * ---
+ * @description a basic implementation of debounce
+ * @param fn a function callback
+ * @param delay a number in milliseconds
+ * @example debounce(fn, 100);
+ */
+export const debounce = (fn: Fn, delay: number, timeout = 0): Fn => (args?: unknown) => {
+  clearTimeout(timeout)
+  // adds `as unknown as number` to ensure setTimeout returns a number
+  // like window.setTimeout
+  timeout = (setTimeout((): void => fn(args), delay) as unknown) as number
+}
