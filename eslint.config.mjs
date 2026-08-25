230edcf472a10c13f6f3adde330c7ea4a5674b5b
Jeff Wainwright <1074042+yowainwright@users.noreply.github.com>
2026-08-25 00:46:08 -0700

    chore(root): improves config

--- eslint.config.mjs
+++
@@ -1,8 +0,0 @@
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
-import eslint from "@eslint/js";
-
-export default [
-  { ignores: ["**/*.ts", "**/dist/*", "*.js", "*.md", "**/node_modules/**"] },
-  eslintPluginPrettierRecommended,
-  eslint.configs.recommended,
-];
